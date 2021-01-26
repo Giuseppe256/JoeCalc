fbe8f03e7e4f9b9c0a564b10614a968009dd2971
Giuseppe256 <massaro.jp256@gmail.com>
2021-01-25 20:15:08 -0600

    Added layout

--- JoeCalc/JoeCalc/MainPage.xaml.cs
+++ JoeCalc/JoeCalc/MainPage.xaml.cs
@@ -14,5 +14,105 @@ namespace JoeCalc
         {
             InitializeComponent();
         }
+
+        void OnClearButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        void OnParenthesesButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        void OnDivideButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        void On7ButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        void On8ButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        void On9ButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        void OnTimesButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        void On4ButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        void On5ButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        void On6ButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        void OnMinusButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        void On1ButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        void On2ButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        void On3ButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        void OnPlusButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        void OnSignButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        void On0ButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        void OnDotButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        void OnEqualsButtonClicked(object sender, EventArgs e)
+        {
+
+        }
     }
 }
