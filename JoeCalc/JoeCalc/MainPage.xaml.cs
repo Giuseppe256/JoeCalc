5967a18bad7f11b1469ce1f363fd3c1b093aeada
Giuseppe256 <massaro.jp256@gmail.com>
2021-01-27 18:05:44 -0600

    Added delete button logic.

--- JoeCalc/JoeCalc/MainPage.xaml.cs
+++ JoeCalc/JoeCalc/MainPage.xaml.cs
@@ -14,7 +14,6 @@ namespace JoeCalc
         String operation = "";
         String operand = "";
         //bool operationPressed = false;
-        int cursorPosition = 0;
 
         public MainPage()
         {
@@ -26,43 +25,50 @@ namespace JoeCalc
         void OnClearButtonClicked(object sender, EventArgs e)
         {
             result.Text = "";
+            operand = "";
+            operation = "";
         }
 
         void OnOperatorButtonClicked(object sender, EventArgs e)
         {
             Button b = (Button)sender;
             operation = b.Text;
+            string formattedOperator = "";
             switch (operation)
             {
                 case "/":
                     operation = "/";
                     //result.Text.Insert(result.CursorPosition, " / ");
-                    result.Text += " / ";
+                    formattedOperator = "/";
                     break;
                 case "X":
                     operation = "*";
                     //result.Text.Insert(result.CursorPosition, " x ");
-                    result.Text += " x ";
+                    formattedOperator = "x";
                     break;
                 case "-":
                     operation = "-";
                     //result.Text.Insert(result.CursorPosition, " - ");
-                    result.Text += " - ";
+                    formattedOperator = "-";
                     break;
                 case "+":
                     operation = "+";
                     //result.Text.Insert(result.CursorPosition, " + ");
-                    result.Text += " + ";
+                    formattedOperator = "+";
                     break;
                 default:
                     break;
             }
 
-            result.CursorPosition += 3;
-            //result.CursorPosition = result.Text.Length;
-
+            int cursorPosition = result.CursorPosition;
+            string str0 = result.Text;
+            string str1 = str0.Substring(0, cursorPosition);
+            string str2 = str0.Substring(cursorPosition);
+            result.Text = str1 + formattedOperator + str2;
+            result.CursorPosition = cursorPosition + 1;
             operand = "";
 
+            //result.CursorPosition = result.Text.Length;
             //value = Double.Parse(result.Text);
             //operationPressed = true;
             //equation.Text = value + " " + operation;
@@ -73,14 +79,28 @@ namespace JoeCalc
 
         }
 
-        void OnDivideButtonClicked(object sender, EventArgs e)
-        {
-
-        }
-
         void OnDeleteButtonClicked(object sender, EventArgs e)
         {
-
+            int cursorPosition = result.CursorPosition;
+            int selectionLength = result.SelectionLength;
+            string str0 = result.Text;
+            if (str0 == "") { }
+            else if (selectionLength > 0)
+            {
+                string str1 = str0.Substring(0, cursorPosition);
+                string str2 = str0.Substring(cursorPosition + selectionLength);
+                str0 = str1 + str2;
+                result.Text = str0;
+                result.CursorPosition = cursorPosition;
+            }
+            else
+            {
+                string str1 = str0.Substring(0, cursorPosition - 1);
+                string str2 = str0.Substring(cursorPosition);
+                str0 = str1 + str2;
+                result.Text = str0;
+                result.CursorPosition = cursorPosition - 1;
+            }
         }
 
         void OnNumButtonClicked(object sender, EventArgs e)
@@ -95,7 +115,7 @@ namespace JoeCalc
             }
             else
             {
-                cursorPosition = result.CursorPosition;
+                int cursorPosition = result.CursorPosition;
                 string str0 = result.Text;
                 string str1 = str0.Substring(0, cursorPosition);
                 string str2 = str0.Substring(cursorPosition);
@@ -103,27 +123,9 @@ namespace JoeCalc
                 operand += b.Text;
                 result.CursorPosition = cursorPosition + 1;
             }
-
-            
-
             //result.Focus();
             //result.Text.Insert(result.CursorPosition, b.Text);
             //result.Text += b.Text;
-        }
-
-        void OnTimesButtonClicked(object sender, EventArgs e)
-        {
-
-        }
-
-        void OnMinusButtonClicked(object sender, EventArgs e)
-        {
-
-        }
-
-        void OnPlusButtonClicked(object sender, EventArgs e)
-        {
-
         }
 
         void OnSignButtonClicked(object sender, EventArgs e)
@@ -133,12 +135,32 @@ namespace JoeCalc
 
         void OnDotButtonClicked(object sender, EventArgs e)
         {
-
+            if (operand.Contains(".")) { }
+            else if (operand == "")
+            {
+                int cursorPosition = result.CursorPosition;
+                string str0 = result.Text;
+                string str1 = str0.Substring(0, cursorPosition);
+                string str2 = str0.Substring(cursorPosition);
+                result.Text = str1 + "0." + str2;
+                operand += "0.";
+                result.CursorPosition = cursorPosition + 2;
+            }
+            else
+            {
+                int cursorPosition = result.CursorPosition;
+                string str0 = result.Text;
+                string str1 = str0.Substring(0, cursorPosition);
+                string str2 = str0.Substring(cursorPosition);
+                result.Text = str1 + "." + str2;
+                operand += ".";
+                result.CursorPosition = cursorPosition + 1;
+            }
         }
 
         void OnEqualsButtonClicked(object sender, EventArgs e)
         {
-
+            
         }
     }
 }
